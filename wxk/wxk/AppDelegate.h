26c0876a89258140718471c51090d56345dad299
XiaoKaiWu16 <wkk16@icloud.com>
2017-05-31 11:01:21 +0800

    吴晓凯

---
+++ wxk/wxk/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  wxk
+//
+//  Created by KK on 17/5/31.
+//  Copyright © 2017年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ wxk/wxk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wxk
+//
+//  Created by KK on 17/5/31.
+//  Copyright © 2017年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
